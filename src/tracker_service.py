8c115917b2c1a3fb9b27f191ebe1335a374a6fc1
nitzanlevy <nitzan05269@gmail.com>
2021-11-17 18:52:03 +0200

    commit

--- src/tracker_service.py
+++ src/tracker_service.py
@@ -2,10 +2,12 @@
 
 #check about the above line ... python/python3
 import rospy
+import numpy as np
 from robot_standing_in_line.srv import TrackerMsg,TrackerMsgResponse
+from sensor_msgs.msg import LaserScan
 
-service_name="line_tracker_service"
-node_name= "line_tracker_service"
+service_name="tracker_service"
+node_name= "tracker_service"
 
 def init_server():
     rospy.init_node(node_name)
@@ -13,11 +15,20 @@ def init_server():
     print("Tracker service is up")
     rospy.spin()
 
-    
+def scan_callback(msg):
+    relevantRanges=msg.ranges[240:540]
+    print(relevantRanges)
+    filtered=[x for x in relevantRanges if x>0.1]
+    minP = np.min(filtered)
+    print(minP,240+filtered.index(minP))
+
 def handle_request(request):
     print("got request, starting to track the person in front ...")
     print("request: ",request)
-    return TrackerMsgResponse(False)
+    sub = rospy.Subscriber('/scan', LaserScan, scan_callback)
+       
+
+    # return TrackerMsgResponse(False)
 
 if __name__ == "__main__":
     init_server()
