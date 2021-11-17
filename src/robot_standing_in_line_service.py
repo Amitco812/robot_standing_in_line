3d4c82e896f2ca924abf4c2ae7f8fef03cdb140e
Amitco812 <amitak@post.bgu.ac.il>
2021-11-17 16:33:24 +0200

    fixed compilation errors

--- src/robot_standing_in_line_service.py
+++ src/robot_standing_in_line_service.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 import rospy
-from line_detector.srv import ser_message
+from line_detector.srv import ser_message,ser_messageRequest
 from robot_standing_in_line.srv import TrackerMsg,TrackerMsgRequest
+
 if __name__ =="__main__":
     rospy.wait_for_service("/line_end_detection")
     while True:
         try:
             line_end_detection = rospy.ServiceProxy("/line_end_detection",ser_message) 
-            line_end_resp = line_end_detection(True)
+            line_end_resp = line_end_detection(ser_messageRequest(True))
             print("response from line end: ",line_end_resp)
             line_tracker = rospy.ServiceProxy("line_tracker_service",TrackerMsg)
             line_tracker(TrackerMsgRequest(True))
