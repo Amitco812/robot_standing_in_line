8854c4a38d88113a505066663ca067bc86eadc23
Amitco812 <amitak@post.bgu.ac.il>
2022-01-05 18:54:37 +0200

    fixed a lot of bugs in line tracker

--- src/tracker_service.py
+++ src/tracker_service.py
@@ -78,7 +78,7 @@ def find_position_by_two_points(x1,y1,x2,y2):
         return x1+dx(dist_threash,m), y1+dy(dist_threash,m)
 
 def find_two_closest_points(laser_msg):
-    threash_to_next_person = 0.6
+    threash_to_next_person = 0.4
     relevantRanges=laser_msg.ranges[start_angel*4:640]
     filtered=[x for x in relevantRanges if x>0.1]
     # code for following person in front 
@@ -105,7 +105,7 @@ def point_on_poly(x,y,m,b):
     return np.abs((m*x +b) - y) <allowed_threash #if point is on the line with at most threash, return True        
 
 def detect_wall(laser_msg,minp1,minp1_deg,minp2,minp2_deg):
-    points_allowed_not_on_poly = 40
+    points_allowed_not_on_poly = 30
     angel_offset = 30 # we look in (min-offset,max+offset) for a wall
     min_wall_deg = np.max([np.min([minp1_deg,minp2_deg]) - angel_offset,0]) #minimum angle to look for a wall, at least zero
     max_wall_deg = np.min([np.max([minp1_deg,minp2_deg]) + angel_offset + points_allowed_not_on_poly/4,180]) #maximum angle to look for a wall, at most 180 (max angel of lidar)
@@ -135,8 +135,8 @@ def detect_wall(laser_msg,minp1,minp1_deg,minp2,minp2_deg):
                 wall_start = first_idx_not_on_poly
                 wall_end = wall_start + wall_len
                 break
-        print("minDist in wall : ",minDist,"angle to wall start: ",wall_start/4)
-        return m,b # return poly of the wall           
+        if points_not_on_poly < points_allowed_not_on_poly:
+            return m,b # return poly of the wall           
     return False,False
 
 def find_people():
@@ -152,7 +152,7 @@ def find_people():
 
 def find_orthogonal_line_through_point(m,x,y):
     slope = -1/m if m != 0 else 0
-    return slope,y-m*x
+    return slope,y-slope*x
 
 
 def handle_request(request):
@@ -162,13 +162,13 @@ def handle_request(request):
     x_last_person,y_last_person,m_wall,b_wall = None,None,None,None
     while True:
         laser_msg = rospy.wait_for_message('/scan', LaserScan, timeout=None)
-        minp1,minp1_deg,minp2,minp2_deg = find_two_closest_points(laser_msg)
+        minp1,minp1_deg,minp2,minp2_deg = find_two_closest_points(laser_msg) #returns angle from the laser view!!
         m_wall,b_wall = detect_wall(laser_msg,minp1,minp1_deg,minp2,minp2_deg)
         print("wall: m: ",m_wall,"b: ",b_wall)
-        x2,y2 = polar_to_cartesian(minp2,minp2_deg)
+        x2,y2 = polar_to_cartesian(minp2,minp2_deg+270)
         if (not isinstance(m_wall,bool)) and point_on_poly(x2,y2,m_wall,b_wall): #if the second point is a wall, break the loop
-            x_last_person,y_last_person = polar_to_cartesian(minp1,minp1_deg)
-            print("x_last_person: ",x_last_person,"y_last_person: ",y_last_person,"on wall: m: ",m_wall,"b_wall: ",b_wall)
+            x_last_person,y_last_person = polar_to_cartesian(minp1,minp1_deg+270)
+            print("x_last_person: ",x_last_person,"y_last_person: ",y_last_person,"and wall is: m: ",m_wall,"b_wall: ",b_wall)
             break
         scan_callback_two_people(minp1,minp1_deg,minp2,minp2_deg)
         loop_rate.sleep()
@@ -177,13 +177,13 @@ def handle_request(request):
     print("loop broken! (second near point is a wall)")
     #while last point is not the wall
     last_point_x,last_point_y = None,None
-    while (last_point_x is not None) and (last_point_y is not None) and (not point_on_poly(last_point_y,last_point_y,m_wall,b_wall)):
+    while ((last_point_x is None) and (last_point_y is None)) or (not point_on_poly(last_point_x,last_point_y,m_wall,b_wall)):
         laser_msg = rospy.wait_for_message('/scan', LaserScan, timeout=None)
         relevantRanges=laser_msg.ranges[start_angel*4:640]
         filtered=[x for x in relevantRanges if x>0.1]
         minp1 = np.min(filtered)
         minp1_deg = (start_angel+relevantRanges.index(minp1)/4)
-        last_point_x,last_point_y = polar_to_cartesian(minp1,minp1_deg)
+        last_point_x,last_point_y = polar_to_cartesian(minp1,minp1_deg+270)
         loop_rate.sleep()
 
     #now we know we are the only ones in front of the wall
@@ -191,6 +191,8 @@ def handle_request(request):
     x_intersect = (b_last_person_and_wall - b_wall)/(m_wall - m_last_person_and_wall) # x = b2 - b1/ m1 - m2
     y_intersect = x_intersect * m_wall + b_wall # y = the x value of some line
     goal = (x_intersect-dx(dist_from_wall,m_last_person_and_wall), y_intersect-dy(dist_from_wall,m_last_person_and_wall))
+    print("m_last_person_and_wall: ",m_last_person_and_wall,"b_last_person_and_wall: ",b_last_person_and_wall,
+    "x_intersect: ",x_intersect,"y_intersect: ",y_intersect,"goal: ",goal)
     move_base(goal,0)
     return TrackerMsgResponse(False)
 
